4bcf9cfb03c4436e1ea62b0cd0e73f5eebbd7243
JSaucourt <jysru@pm.me>
2023-05-04 10:07:56 +0200

    Added supports

--- speckleret/__init__.py
+++ speckleret/__init__.py
@@ -1,6 +1,7 @@
 from . import metrics
 from . import plots
 from . import retrieval
+from . import supports
 from . import transforms
 
 __version__ = "0.1"
---
+++ speckleret/supports.py
@@ -0,0 +1,47 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def pixels_meshgrids(size: int, center: bool = True, return_polar: bool = True):
+    grid = np.arange(start=0, stop=size)
+    if center:
+        grid = grid - float(size) / 2 + 0.5
+    print(grid)
+    X, Y = np.meshgrid(grid, grid)
+
+    if return_polar:
+        R = np.sqrt(np.square(X) + np.square(Y))
+        T = np.arctan2(Y, X)
+        return (X, Y, R, T)
+    else:
+        return (X, Y)
+    
+
+def square_support(array: np.ndarray, size: int, offsets: tuple[float, float] = None):
+    support = np.zeros(array.shape, dtype=bool)
+    X, Y = pixels_meshgrids(array.shape[0], center=True, return_polar=False)
+    if offsets is None:
+        support[np.logical_and(np.abs(X) <= size/2, np.abs(Y) <= size/2) ] = True
+    else:
+        support[np.logical_and(np.abs(X - offsets[0]) <= size/2, np.abs(Y - offsets[1]) <= size/2) ] = True
+    return support
+    
+
+def disk_support(array: np.ndarray, radius: int, offsets: tuple[float, float] = None):
+    support = np.zeros(array.shape, dtype=bool)
+    X, Y, R, _ = pixels_meshgrids(array.shape[0], center=True)
+    if offsets is None:
+        support[R <= radius] = True
+    else:
+        support[np.sqrt(np.square(X - offsets[0]) + np.square(Y - offsets[1])) <= radius] = True
+    return support
+
+
+
+if __name__ == "__main__":
+    arr = np.zeros((14,14))
+    sup1 = disk_support(arr, radius=4, offsets=(1,-2))
+    sup2 = square_support(arr, size=4, offsets=(-1,1))
+    
+    plt.imshow(sup1)
+    plt.show()
