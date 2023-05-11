0920d7206953e8e5a9373f0769596b4c64eec2ab
JSaucourt <jysru@pm.me>
2023-05-11 18:47:43 +0200

    Added ref to max by default

--- speckleret/plots.py
+++ speckleret/plots.py
@@ -16,11 +16,15 @@ def complex_to_hsv(z, rmin, rmax, hue_start=0):
     return hsv_to_rgb(np.dstack((h,s,v)))
 
 
-def complex_imshow(field, figsize: tuple[int,int] = (15,5), remove_ticks: bool = False):
+def complex_imshow(field, figsize: tuple[int,int] = (15,5), remove_ticks: bool = False, ref: tuple[int,int] = None):
+    if ref is None:
+        ref = np.argmax(np.abs(field))
+        ref = np.unravel_index(ref, field.shape)
+
     fig, axs = plt.subplots(1, 3, figsize=figsize)
     pl0 = axs[0].imshow(np.abs(field), cmap='gray')
-    pl1 = axs[1].imshow(np.angle(field), cmap='hsv')
-    pl2 = axs[2].imshow(complex_to_hsv(field, rmin=0, rmax=np.max(np.abs(field))))
+    pl1 = axs[1].imshow(np.angle(field * np.exp(-1j * np.angle(field[ref[0], ref[1]]))), cmap='hsv')
+    pl2 = axs[2].imshow(complex_to_hsv(field * np.exp(-1j * np.angle(field[ref[0], ref[1]])), rmin=0, rmax=np.max(np.abs(field))))
     pls = [pl0, pl1, pl2]
 
     _ = axs[0].set_title("Amplitude")
