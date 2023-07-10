e6909c998c7d5d35fe31cd9f91c5a5bd917b90a2
JSaucourt <jysru@pm.me>
2023-07-10 17:36:47 +0200

    Added AltProjPR class

--- speckleret/retrievalmath.py
+++ speckleret/retrievalmath.py
@@ -214,24 +214,160 @@ def report_convergence_results(results: dict, yscale: str = 'log'):
     plt.legend()
 
 
-class PhaseRetrieval():
+class AltProjPR():
+    default_direct_transform: callable = transforms.fourier_transform
+    default_inverse_transform: callable = transforms.inverse_fourier_transform
+    apply_planes_magnitudes: list[bool, bool] = [True, True]
+    apply_planes_supports: list[bool, bool] = [True, True]
 
-    def __init__(self) -> None:
-        self.targets = None
-        self.magnitudes = None
-        self.supports = None
-        self.inits = None
-        self.algorithm = None
-        self.algorithm_kwargs = None
-        self.max_iter = None
-        self.rel_tol = None
-        pass
+    def __init__(
+        self,
+        magnitudes: list[np.ndarray, np.ndarray],
+        targets: list[np.ndarray, np.ndarray] = None,
+        supports: list[np.ndarray, np.ndarray] = None,
+        direct_transform: callable = AltProjPR.default_direct_transform,
+        inverse_transform: callable = AltProjPR.default_inverse_transform,
+        apply_planes_magnitudes: list[bool, bool] = [AltProjPR.apply_planes_magnitudes]
+        apply_planes_supports: list[bool, bool] = [AltProjPR.apply_planes_supports]
+        ) -> None:
 
-    def run(self) -> None:
+        self._direct_transform: callable = None
+        self._inverse_transform: callable = None
+        self._magnitudes: list[np.ndarray, np.ndarray] = None
+        self._targets: list[np.ndarray, np.ndarray] = None
+        self._supports: list[np.ndarray, np.ndarray] = None
+        self._init: np.ndarray = None
+        self._algorithm: callable = None
+        self._algorithm_kwargs: dict = None
+        self._max_iter: int = None
+        self._rel_tol: float = None
+        self._apply_planes_magnitudes: list[bool, bool] = apply_planes_magnitudes
+        self._apply_planes_supports: bool = apply_planes_supports
+
+        self._set_transforms(direct_transform, inverse_transform)
+        self._set_magnitudes(magnitudes)
+
+    def _set_transforms(self, direct_transform: callable, inverse_transform: callable) -> None:
+        are_valid = self._check_transforms(direct_transform, inverse_transform)
+        if are_valid:
+            self._direct_transform = direct_transform
+            self._inverse_transform = inverse_transform
+        else:
+            raise TypeError("Transforms must be of callable type!")            
+
+    def _check_transforms(self, *args) -> bool:
+        is_valid: bool = True
+        for i in range(args):
+            is_valid = is_valid & callable(args[i])
+        return is_valid
+
+    def _set_magnitudes(self, magnitudes[i]: list[np.ndarray, np.ndarray]) -> None:
+        self._magnitudes = self._check_magnitudes(magnitudes)
+
+    def _check_magnitudes(self, magnitudes: list[np.ndarray, np.ndarray]) -> None:
+        for i in range(len(magnitudes)):
+            if np.iscomplexobj(magnitudes[i]):
+                magnitudes[i] = np.abs(magnitudes[i])
+        return magnitudes
+
+    def _apply_magnitude(self, x: np.ndarray, idx: int) -> np.ndarray
+        if self.magnitudes is not None:
+            x = self._magnitudes[idx] * np.exp(1j * np.angle(x))
+        return x
+
+    def _apply_support(self, x: np.ndarray, idx: int) -> np.ndarray
+        if self._supports is not None:
+            x[np.logical_not(self._supports[idx])] = 0
+        return x
+
+    def _proj_s(self, x: np.ndarray) -> np.ndarray:
+        """Plane1: Object plane projection and support application"""
+        x = self._apply_magnitude(x, idx=0) if self._apply_planes_magnitude[0] else x
+        return self._apply_support(x, idx=0) if self._apply_planes_supports[0] else x
+
+    def _proj_m(self, x: np.ndarray) -> np.ndarray:
+        """Plane2: Fourier magnitude projection and support application"""
+        x = self._direct_transform(x)
+        x = self._apply_magnitude(x, idx=1) if self._apply_planes_magnitude[1] else x
+        x = self._apply_support(x, idx=1) if self._apply_planes_supports[1] else x
+        return self._inverse_transform(x)
+
+    def _refl_s(self, x: np.ndarray, g: float) -> np.ndarray:
+        """Plane1: Object plane reflection and support application"""
+        return (1 + g) * self._proj_s(x) - g * x
+
+    def _refl_m(self, x: np.ndarray, g: float) -> np.ndarray:
+        """Plane1: Object plane reflection and support application"""
+        return (1 + g) * self._proj_m(x) - g * x
+
+    def _iter_ER(self, x: np.ndarray) -> np.ndarray:
+        """Error Reduction algorithm iteration"""
+        return self._proj_s(self._proj_m(x))
+
+    def _iter_SF(self, x: np.ndarray) -> np.ndarray:
+        """Solvent Flipping algorithm iteration"""
+        return self._refl_s(self._proj_m(x), g=1)
+
+    def _iter_HIO(self, x: np.ndarray, beta: float = 0.7) -> np.ndarray:
+        """Hybrid Input-Output algorithm iteration"""
+        x = x.copy()
+        x_proj = self._proj_m(x)
+        x[self._supports[0]] = proj[self._supports[0]]
+        x[np.logical_not(self._supports[0])] = x[np.logical_not(self._supports[0])] - beta * self._proj_s(x_proj)[np.logical_not(self._supports[0])]
+        return x
+
+    def _iter_ASR(self, x: np.ndarray) -> np.ndarray:
+        """Averaged succesive reflections algorithm iteration"""
+        return 0.5 * (self._refl_s(self._refl_m(x, g=1), g=1) + x)
+
+    def _iter_HPR(self, x: np.ndarray, beta: float = 0.7) -> np.ndarray:
+        """Hybrid projection reflection algorithm iteration"""
+        x_proj = self._proj_m(x)
+        x1 = self._refl_s(self._refl_m(x, g=1) + (beta - 1) * x_proj, g=1)
+        x2 = (1 - beta) * x_proj
+        return 0.5 * (x1 + x_proj + x2)
+
+    def _iter_DM(self, x: np.ndarray, beta: float = 0.7, gs: float = None, gm: float = None) -> np.ndarray:
+        """Difference Maps algorithm iteration"""
+        # Random projections and the optimization of an algorithm for phase retrieval, J. Phys. A-Math Gen, Vol 36 pp 2995-3007
+        if gs is None:
+            gs = 1 / beta
+        if gm is None:
+            gm = -1 / beta
+        
+        x_pmrs = self._proj_m(self._refl_s(x, g=gs))
+        x_psrm = self._proj_s(self._refl_m(x, g=gm))
+        return x + beta * (x_pmrs - x_psrm)
+
+    def _iter_RAAR(self, x: np.ndarray, beta: float = 0.7) -> np.ndarray:
+        """Relaxed Averaged Alternating Reflectors algorithm iteration"""
+        return beta * _iter_ASR(x) + (1 - beta) * self._proj_m(x)   
+
+    def run(
+        self,
+        algorithm: callable,
+        algorithm_kwargs: dict = None,
+        init: np.ndarray = None,
+        max_iter: int = 100,
+        rel_tol: float = 1e-3,
+        ) -> None:
+
+        x, results = spr.retrievalmath.run(
+            magnitudes=(np.abs(field), np.abs(ft)),
+            support=support,
+            init=spr.initializers.random_phases(magnitude=np.abs(field)),
+            algorithm=spr.retrievalmath.RAAR,
+            algorithm_kwargs={'beta': 0.7},
+            max_iter=100,
+            rel_tol=1e-6,
+            )
+
+    def define_support(self) -> None:
         pass
 
     def show(self) -> None:
         pass
+
 
 
 
