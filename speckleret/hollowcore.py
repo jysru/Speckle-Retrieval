f1cd754ccb0c18497fe2a2e3cd315b4824f077f5
JSaucourt <jysru@pm.me>
2025-06-17 15:13:52 +0200

    Update hollowcore.py

--- speckleret/hollowcore.py
+++ speckleret/hollowcore.py
@@ -1,1 +1,14 @@
+import os
+import numpy as np
+from scipy.io import loadmat
 
+def build_modal_base(path: str):
+    lp01 = loadmat(os.path.join())['data']
+
+
+
+# def get_unit_energy_mode()
+
+
+
+path = r'C:\Users\saucourt\Downloads\T12 L8407 fields'
